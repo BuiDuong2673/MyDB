ce4e7891989b0306ac95dab24db745a92ec8e6db
BuiDuong2673 <bachduong26723@gmail.com>
2026-03-22 09:50:16 +0100

    add logics to the account signin and signout

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,8 @@ const nextConfig: NextConfig = {
   experimental: {
     reactCompiler: true,
   },
+  /** Hides the dev-only indicator menu (Route, Turbopack, etc.) that overlaps the bottom of the UI. */
+  devIndicators: false,
 };
 
 export default nextConfig;
